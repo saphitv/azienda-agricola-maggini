7f3b1567da8b9ed8c0fd6845e12e8a55d501ffe3
saphitv <simonmaggini@gmail.com>
2024-09-04 18:58:29 +0200

    cambiato favicon progetto

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -28,6 +28,9 @@ export default function RootLayout({
 }>) {
     return (
         <html lang="en">
+        <head>
+            <link rel="icon" href="/Grapes%20icon.svg>" sizes="any" />
+        </head>
         <body
             className={cn(
                 "bg-background font-sans antialiased",
