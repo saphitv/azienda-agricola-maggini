40dcdcf65abe41c3d0573c2a15d3871285df6a80
Simon Maggini <simonmaggini@gmail.com>
2025-07-22 20:00:30 +0200

    feat: trying better build mode

--- src/lib/http-db/index.ts
+++ src/lib/http-db/index.ts
@@ -3,7 +3,7 @@ import axios from "axios";
 
 export const db = drizzle(async (sql, params, method) => {
     try {
-        const rows = await axios.post(`http://${process.env.PROD == 'true' ? 'http-db' : 'localhost'}:3001/query`, { sql, params, method });
+        const rows = await axios.post(`http://${process.env.PROD == 'true' ? 'http-db' : 'localhost'}:9101/query`, { sql, params, method });
         return { rows: rows.data };
     } catch (e: any) {
         console.error('Error from mysql proxy server: ', e.response.data)
